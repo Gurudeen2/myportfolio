de25ffbfa46e9e2fae99f99197c0983244a76a9c
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-06-17 14:37:51 +0100

    gh-pages setup on package.json

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
 	return (
 		<div className="App">
 			<Routes>
-				<Route path="/" exact element={<Homepage />} />
+				<Route path="/"  element={<Homepage />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/projects" element={<Projects />} />
 				{/* <Route path="/articles" element={<Articles />} /> */}
 				{/* <Route path="/article/:slug" element={<ReadArticle />} /> */}
 				<Route path="/contact" element={<Contact />} />
-				<Route path="*" element={<Notfound />} />
+				<Route path="*" element={<Homepage />} />
 			</Routes>
 		</div>
 	);
