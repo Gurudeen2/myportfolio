638ef78920f4bf88b462aa460a8787a3e1b02a30
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-06-29 23:12:54 +0100

    update description

--- src/data/user.js
+++ src/data/user.js
@@ -20,7 +20,7 @@ const INFO = {
 	homepage: {
 		title: "Full-Stack Web Developer",
 		description:
-			"I am a backend developer with expertise in Django (python) and Reactjs. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a fullstack developer with expertise in Django (python) and Reactjs. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
 	about: {
